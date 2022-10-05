627e5c6abdb634bcd5d7d050a4adb0a09952c78c
jasito12345 <113585574+jasito12345@users.noreply.github.com>
2022-10-05 11:17:36 -0400

    Add files via upload

---
+++ Problema5/pregunta5/Program.fs
@@ -0,0 +1,29 @@
+﻿// Más información acerca de F# en http://fsharp.net
+// Vea el proyecto "Tutorial de F#" para obtener más ayuda.
+
+open System
+
+let suma a b = a + b
+let resta a b = a - b
+let multiplicacion a b = a * b
+let division a b = float (a / b)
+
+[<EntryPoint>]
+let main argumentos = 
+    
+    Console.WriteLine("Introduzca dos numeros")
+    let num1 = Console.ReadLine()
+    let num2 = Console.ReadLine()
+    let valor1 = Convert.ToInt32(num1)
+    let valor2 = Convert.ToInt32(num2)
+    let valor3 = suma valor1 valor2
+    Console.WriteLine("la suma es: "+valor3.ToString())
+    let valor4 = resta valor1 valor2
+    Console.WriteLine("la resta es: "+valor4.ToString())
+    let valor5 = multiplicacion valor1 valor2
+    Console.WriteLine("la multiplicaion es: "+valor5.ToString())
+    let valor6 = float (division valor1 valor2)
+    Console.WriteLine("la division es: "+valor6.ToString())
+
+    let tecla = Console.ReadKey()
+    0 // devolver un código de salida enterover un código de salida entero
